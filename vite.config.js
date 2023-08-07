f20adf145e8c1dd370c310e2149bb64ba0a3dabc
TreantProtectorGo <wing.ws.lee@gmail.com>
2023-08-07 15:05:59 +0800

    1

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: isProduction ? '/3D/' : '/'
+  };
+});
